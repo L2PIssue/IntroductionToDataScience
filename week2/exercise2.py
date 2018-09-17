038bc043119097ffb19b3722ed6ea26e3a4867c3
Miiu <miia.ramo@helsinki.fi>
2018-09-17 21:25:44 +0300

    exercise 2 partly completed

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -5,7 +5,23 @@ data = pd.read_csv('cleaned_data.csv')
 # First consider each feature variable in turn. For categorical variables, 
 # find out the most frequent value, i.e., the mode. For numerical variables, 
 # calculate the median value.
-print(data.describe(include='all'))
+mode_survived = data['Survived'].mode()
+print(mode_survived)
+mode_pclass = data['Pclass'].mode()
+print(mode_pclass)
+mode_sex = data['Sex'].mode()
+print(mode_sex)
+mode_embarked = data['Embarked'].mode()
+print(mode_embarked)
+mode_deck = data['Deck'].mode()
+print(mode_deck)
+
+mean_age = data['Age'].mean()
+print(mean_age)
+mean_sibsp = data['SibSp'].mean()
+print(mean_sibsp)
+mean_parch = data['Parch'].mean()
+print(mean_parch)
 
 # Combining the modes of the categorical variables, and the medians of the numerical 
 # variables, construct an imaginary “average survivor” on board of the ship. 
@@ -35,3 +51,7 @@ print(data.describe(include='all'))
 # Can you say something about the effect of the choices that were made, 
 # in particular, to use the mode or the mean to impute missing values, 
 # instead of, for example, ignoring passengers with missing data?
+    # 1.  Since a lot of the passengers are missin data, I'd say ignoring those passengers
+    #     completely is not such a good idea (too little data).
+    # 2.  Only a small number of the passengers have info about the Cabin 
+    #     (and thus the Deck) so I'd think it might be wiser to ignore the Cabin data.
